21aab80a678f1b1e84161de480e73d134ae4655e
gaptop <dkdlwkrsbxjs@naver.com>
2023-09-19 21:27:46 +0900

    day39 + sc

---
+++ inflearn_codingtest_python/section4/4n2.py
@@ -0,0 +1,11 @@
+def solution(moves):
+    x = y = 0
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+    
+    return [x, y]
+                            
+print(solution('RRRDDDLU'))
+print(solution('DDDRRRDDLL'))
+print(solution('RRRRRRDDDDDDUULLL'))
+print(solution('RRRRDDDRRDDLLUU'))
