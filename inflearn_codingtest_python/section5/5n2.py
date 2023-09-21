11fbe097c8cc7c23f85908ef6d3e39aa44499a0f
gaptop <dkdlwkrsbxjs@naver.com>
2023-09-21 22:06:41 +0900

    day41

--- inflearn_codingtest_python/section5/5n2.py
+++ inflearn_codingtest_python/section5/5n2.py
@@ -13,9 +13,6 @@ def solution(nums):
         bin = [nums[i], nums[i+1]]
         if nums[i+1] - nums[i] == lim:
             answer += [[nums[i], nums[i+1]]]
-
-        
-
                 
     return answer
 
@@ -25,3 +22,18 @@ print(solution([5, 10, 15, 20, 25, 11]))
 print(solution([2, 4, 3, 1, 5, 7, 8, 12, 13, 15, 23]))
 print(solution([100, 200, 300, 400, 120, 130, 135, 132, 121]))
 
+def solution(nums):
+    answer = []
+    n = len(nums)
+    minN = 1000000000
+    nums.sort()
+    for i in range(1, n):
+        diff = nums[i] - nums[i-1]
+        minN = min(minN, diff)
+
+    for i in range(1, n):
+        diff = nums[i] - nums[i-1]
+        if diff == minN:
+            answer.append([nums[i-1], nums[i]])
+             
+    return answer
