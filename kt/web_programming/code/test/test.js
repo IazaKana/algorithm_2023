d5c805c330738b608c6e65eb067325dd8781035c
gaptop <dkdlwkrsbxjs@naver.com>
2024-05-08 23:16:23 +0900

    FD_arrange

---
+++ kt/web_programming/code/test/test.js
@@ -0,0 +1,4 @@
+console.log("hello kt aivle!");
+console.log(1);
+console.log(2);
+console.log(3);
---
+++ kt/web_programming/code/test/test.py
@@ -0,0 +1,9 @@
+print('hello kt aivle!')
+def echo(msg):
+    print(msg)
+
+1, 11
+2, 11
+3, 11
+4, 11
+5, 11
